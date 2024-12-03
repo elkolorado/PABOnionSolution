ae710a1a6971a42e9259d7dd97eed96482662070
Filip Zieliński <filip.zielinski@microsoft.wsei.edu.pl>
2024-12-03 19:35:34 +0100

    fix: https cert

--- RazorPages/Program.cs
+++ RazorPages/Program.cs
@@ -9,10 +9,17 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+.ConfigureWebHostDefaults(webBuilder =>
+{
+    webBuilder.UseStartup<Startup>()
+              .ConfigureKestrel(options =>
+              {
+                  options.ConfigureEndpointDefaults(listenOptions =>
+                  {
+                      //listenOptions.UseHttps(); // Remove or comment out this line to disable HTTPS
+                  });
+              });
+});
 
 
 }
